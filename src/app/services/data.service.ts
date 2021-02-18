68e0fc3b146a0f4821eb38cd2a398e95d5f88a63
Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-02-17 20:10:29 -0600

    Add new project

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+  getPokemons(){
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=50`);
+  }
+  getMorePokemons(name:string){
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  }
+}
